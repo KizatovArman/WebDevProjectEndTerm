5c615915fdadf3870df79d5dd86714eac0824e72
showmie <cblueski@gmail.com>
2019-05-07 15:17:07 +0600

    frerefrerer

--- WebDevEzTeam/projectenv/backend/api/views/auth.py
+++ WebDevEzTeam/projectenv/backend/api/views/auth.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from api.serializers import UserSerializer
+from apip.serializers import UserSerializer
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
@@ -8,3 +8,40 @@ from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework.views import APIView
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser,]
+
+
+@api_view(['POST'])
+def login(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data.get('user')
+    token, created = Token.objects.get_or_create(user=user)
+    print(request.user)
+    return Response({'token': token.key, 'user_id': user.id})
+
+
+@api_view(['POST'])
+def logout(request):
+    # print(request.auth)
+    request.auth.delete()
+    print(request.user)
+    return Response(status=status.HTTP_200_OK)
+
+
+class UserCreate(APIView):
+    def post(self, request, format='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
