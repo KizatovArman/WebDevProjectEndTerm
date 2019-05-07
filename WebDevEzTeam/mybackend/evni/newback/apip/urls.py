bd0de711bc77079708cfcac133f00a1a147f8661
Kizatov Arman <31284676+KizatovArman@users.noreply.github.com>
2019-05-07 13:14:18 +0600

    strange

--- WebDevEzTeam/mybackend/evni/newback/apip/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from apip import views
-
-urlpatterns =[
-    path('exercises_categories/', views.ExerciseCategoryListAllowAny.as_view()),
-    path('login/', views.login),
-    path('logout/', views.logout),
-    path('users/', views.UserList.as_view()),
-]
